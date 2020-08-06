b9608e4907c7ad303d639a7a913fecbc05661457
Lucas Koerner <lcskoerner@gmail.com>
2020-08-06 00:42:38 -0400

    bootstra installed & edit action done

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'faker'
+gem 'simple_form'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
+
   def index
+    @articles = Article.all
   end
 
   def create
@@ -13,10 +16,24 @@ class ArticlesController < ApplicationController
 
   def show
   end
-  
+
   def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
   end
 
   def destroy
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
